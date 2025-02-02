db67c8929f28d5456f71c201cc7fd33da1846874
marcoschags <marcoschags@hotmail.com>
2025-02-02 12:13:28 -0300

    gpio_set_irq_enabled_with_callback()

--- TarefaAnimacao.c
+++ TarefaAnimacao.c
@@ -13,6 +13,10 @@
 #define BOTAO_A 5
 #define BOTAO_B 6
 
+volatile int contador = 0; // Variável global para armazenar o contador
+PIO pio;
+uint sm;
+
 uint32_t matrix_rgb(double r, double g, double b) {
     unsigned char R = r * 255;
     unsigned char G = g * 255;
@@ -26,65 +30,62 @@ void mostrar_numero(int num, PIO pio, uint sm) {
          1, 0, 0, 0, 1,
          1, 0, 0, 0, 1,
          1, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}, // 0
-        
+         1, 1, 1, 1, 1},
         {0, 0, 1, 0, 0,
          0, 1, 1, 0, 0,
          1, 0, 1, 0, 0,
          0, 0, 1, 0, 0,
-         1, 1, 1, 1, 1}, // 1
-        
+         1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
          1, 1, 1, 1, 1,
          1, 0, 0, 0, 0,
-         1, 1, 1, 1, 1}, // 2
-        
+         1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
          0, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}, // 3
-        
+         1, 1, 1, 1, 1},
         {1, 0, 0, 1, 1,
          1, 0, 0, 1, 1,
          1, 1, 1, 1, 1,
          0, 0, 0, 1, 1,
-         0, 0, 0, 1, 1}, // 4
-        
+         0, 0, 0, 1, 1},
         {1, 1, 1, 1, 1,
          1, 0, 0, 0, 0,
          1, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}, // 5
-        
+         1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1,
          1, 0, 0, 0, 0,
          1, 1, 1, 1, 1,
          1, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}, // 6
-        
+         1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
          0, 0, 1, 1, 0,
          0, 0, 1, 0, 0,
-         0, 1, 0, 0, 0}, // 7
-        
+         0, 1, 0, 0, 0},
         {1, 1, 1, 1, 1,
          1, 0, 0, 0, 1,
          1, 1, 1, 1, 1,
          1, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}, // 8
-        
+         1, 1, 1, 1, 1},
         {1, 1, 1, 1, 1,
          1, 0, 0, 0, 1,
          1, 1, 1, 1, 1,
          0, 0, 0, 0, 1,
-         1, 1, 1, 1, 1}  // 9
+         1, 1, 1, 1, 1}
     };
     for (int i = 0; i < NUM_PIXELS; i++) {
         pio_sm_put_blocking(pio, sm, matrix_rgb(numeros[num][24 - i], 0, 0));
     }
+}
+
+void gpio_callback(uint gpio, uint32_t events) {
+    if (gpio == BOTAO_A && contador < 9) contador++;
+    if (gpio == BOTAO_B && contador > 0) contador--;
+    mostrar_numero(contador, pio, sm);
 }
 
 int main() {
@@ -98,31 +99,17 @@ int main() {
     gpio_set_dir(BOTAO_B, GPIO_IN);
     gpio_pull_up(BOTAO_B);
     
-    PIO pio = pio0;
+    pio = pio0;
     uint offset = pio_add_program(pio, &TarefaAnimacao_program);
-    uint sm = pio_claim_unused_sm(pio, true);
+    sm = pio_claim_unused_sm(pio, true);
     TarefaAnimacao_program_init(pio, sm, offset, OUT_PIN);
     
-    int contador = 0;
-    bool estado_led = false;
+    gpio_set_irq_enabled_with_callback(BOTAO_A, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    gpio_set_irq_enabled_with_callback(BOTAO_B, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     
     while (true) {
-        estado_led = !estado_led;
-        gpio_put(LED_VERMELHO, estado_led);
-        
-        if (!gpio_get(BOTAO_A)) {
-            if (contador < 9) contador++;
-            mostrar_numero(contador, pio, sm);
-            sleep_ms(200);
-        }
-        
-        if (!gpio_get(BOTAO_B)) {
-            if (contador > 0) contador--;
-            mostrar_numero(contador, pio, sm);
-            sleep_ms(200);
-        }
-        
-        sleep_ms(50);
+        gpio_put(LED_VERMELHO, !gpio_get(LED_VERMELHO));
+        sleep_ms(500);
     }
     return 0;
 }
